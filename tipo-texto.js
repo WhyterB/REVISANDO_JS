c809ae036069a53be2c9de010f457fe4a4a823f2
WhyterB <matheusborinsoares@gmail.com>
2024-03-13 12:34:57 -0300

    Subir repositorio

---
+++ tipo-texto.js
@@ -0,0 +1,10 @@
+const nomeAluno="Matheus";
+const maeAluno="Solange";
+
+console.log(nomeAluno);
+
+const idadeAluno=17;
+
+console.log(nomeAluno*idadeAluno);
+
+console.log(nomeAluno+maeAluno);//concatenação
