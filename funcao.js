ba283be6445e30012974e485a1bee96a6d594b98
WhyterB <matheusborinsoares@gmail.com>
2024-03-15 12:12:46 -0300

    Finalizar revisão

---
+++ funcao.js
@@ -0,0 +1,49 @@
+//função não parametrizada
+//const n1=5;
+//const n2=100;
+//function Soma(){
+//    return n1+n2;
+//}
+
+//function Multiplica(){
+//    return n1*n2;
+//}
+//console.log(Soma());
+//console.log(Multiplica());
+// function Soma(n3=0,n4=0){
+//     return n3+n4;
+// }
+
+// console.log(Soma(6,5));
+
+//função anônima
+// const f1=function(v1,v2){
+//     return v1/v2;
+// }
+// console.log(f1(12,25))
+
+//vetores
+
+// let mulheres=["Poliana","Camila","Amanda"];
+// console.log(mulheres);
+// mulheres.map((el)=>{
+//     console.log(el);
+// })
+
+//função construtor anônima
+const f2= new Function('v3','v4','return v3-v4');
+console.log(f2(7,3));
+
+//sem arrow function
+const imc=function(PESO,ALTURA){
+    return PESO/(ALTURA*ALTURA);
+}
+
+console.log(imc(55,1.68));
+
+// com arrow function
+const imc2=function(PESO,ALTURA)=>{
+    return PESO/(ALTURA*ALTURA);
+}
+
+console.log(imc(55,1.68));
